416700efdba1b9a1f463b9bb4d89b77110cf3c92
ugljanin <emirugljanin@gmail.com>
2016-10-24 17:23:07 +0200

    cxc

--- db_fns.php
+++ db_fns.php
@@ -5,7 +5,7 @@ function db_connect()
 	$user=getenv('MYSQL_USER');
 	$password=getenv('MYSQL_PASSWORD');
 
-	$result=new mysqli('127.0.0.1','root','zakaria','baza','3306');
+	$result=new mysqli('127.0.0.1','root','zakaria','baza','3306','/cloudsql/datahouse');
 
 	//$result=new mysqli($dsn,$user,$password);
 
