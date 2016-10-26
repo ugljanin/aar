6bc96e66247747c60e28c3519c343cb2d614a7f2
ugljanin <emirugljanin@gmail.com>
2016-10-26 23:22:58 +0200

    dd

--- post.php
+++ post.php
@@ -7,44 +7,31 @@ $conn=db_connect();
 $time=time();
 $timedate=date('Y-m-d H:s:i',$time);
 
-/*
-$sql="select * from configuration where configurationid=1";
+
+$sql="select * from gateways where accesstoken='$_POST['accesstoken']'";
 $result=mysqli_query($conn,$sql);
-
-$podesavanje=mysqli_fetch_array($result,MYSQLI_ASSOC);
-
-if($_POST[accesstoken]==$podesavanja[accesstoken])
+if(mysqli_num_rows($result)=='1')
 {
-	$results = print_r($_POST, true);
-	$myfile = fopen("post.txt", "w") or die("Unable to open file!");
-	fwrite($myfile, $_POST['json']);
-	fclose($myfile);
-}
-else
-{
-	$results = print_r($_POST, true);
-	$myfile = fopen("error.txt", "w") or die("Unable to open file!");
-	fwrite($myfile, $_POST['json']);
-	fclose($myfile);
-}
-*/
-$myfile = fopen("gs://aar-framework.appspot.com/post.txt", 'w');
 
+	$gateway=mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+	$myfile = fopen("gs://aar-framework.appspot.com/post.txt", 'w');
 	//$myfile = fopen("post.txt", "w") or die("Unable to open file!");
 	fwrite($myfile, $_POST['json']);
 	fclose($myfile);
 
-$json_a = json_decode($_POST['json'], true);
+	$json_a = json_decode($_POST['json'], true);
 
-foreach($json_a['Details'] as $date)
-{
-	 $currdate=$date['date'];
+	foreach($json_a['Details'] as $date)
+	{
+		 $currdate=$date['date'];
+	}
+
+	foreach($json_a['SensorData'] as $data)
+	{
+			$sql="insert into sensordata (name, value, date,gatewayid) values ('".$data[name]."','".$data[value]."','".$currdate."','".$gateway[gatewayid]."')";
+			$result=mysqli_query($conn,$sql);
+	}
+
 }
-
-foreach($json_a['SensorData'] as $data)
-{
-		$sql="insert into sensordata (name, value, date) values ('".$data[name]."','".$data[value]."','".$currdate."')";
-		$result=mysqli_query($conn,$sql);
-}
-
 ?>
