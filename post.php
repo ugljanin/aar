cef72c162168fbc7c4949e01c117bdb171fb3dd5
ugljanin <emirugljanin@gmail.com>
2016-10-26 18:37:25 +0200

    added

--- post.php
+++ post.php
@@ -1,7 +1,27 @@
 <?php
 
-$results = print_r($_POST, true);
-$myfile = fopen("post.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $results);
-fclose($myfile);
+include("fns.php");
+
+date_default_timezone_set('UTC');
+$sql="select * from configuration where configurationid=1";
+$result=mysqli_query($conn,$sql);
+
+$podesavanje=mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+if($_POST[accesstoken]==$podesavanja[accesstoken])
+{
+	$results = print_r($_POST, true);
+	$myfile = fopen("post.txt", "w") or die("Unable to open file!");
+	fwrite($myfile, $results);
+	fclose($myfile);
+}
+else
+{
+	$results = print_r($_POST, true);
+	$myfile = fopen("error.txt", "w") or die("Unable to open file!");
+	fwrite($myfile, $results);
+	fclose($myfile);
+}
+
+
 ?>
