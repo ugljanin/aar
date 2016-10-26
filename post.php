019f14a977a1303d759a99e70812e8bc6125a117
ugljanin <emirugljanin@gmail.com>
2016-10-26 19:25:22 +0200

    fff

--- post.php
+++ post.php
@@ -1,8 +1,15 @@
 <?php
-
 include("fns.php");
 
 date_default_timezone_set('UTC');
+
+$title="Configure gateway";
+include "header.php";
+
+$conn=db_connect();
+$time=time();
+$timedate=date('Y-m-d H:s:i',$time);
+
 $sql="select * from configuration where configurationid=1";
 $result=mysqli_query($conn,$sql);
 
@@ -12,16 +19,29 @@ if($_POST[accesstoken]==$podesavanja[accesstoken])
 {
 	$results = print_r($_POST, true);
 	$myfile = fopen("post.txt", "w") or die("Unable to open file!");
-	fwrite($myfile, $results);
+	fwrite($myfile, $_POST['json']);
 	fclose($myfile);
 }
 else
 {
 	$results = print_r($_POST, true);
 	$myfile = fopen("error.txt", "w") or die("Unable to open file!");
-	fwrite($myfile, $results);
+	fwrite($myfile, $_POST['json']);
 	fclose($myfile);
 }
 
+$json_a = json_decode($_POST['json'], true);
+
+foreach($json_a['Detail'] as $date)
+{
+	 $currdate=$date[date];
+}
+
+foreach($json_a['SensorData'] as $data)
+{
+		$sql="insert into sensordata (name, value, date) values ('".$data[name]."','".$data[value]."','".$currdate."')"
+		$result=mysqli_query($conn,$sql);
+}
+?>
 
 ?>
