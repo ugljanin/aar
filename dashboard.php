9d1bb9e8fcb9493c9eff144ede3944a0826c8aea
ugljanin <emirugljanin@gmail.com>
2016-11-27 21:25:25 +0100

    hhh

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@ include("fns.php");
 
 //date_default_timezone_set('UTC');
 
-$title="ICU temperature and humidity monitoring and control";
+$title="Dashboard";
 include "header.php";
 
 $conn=db_connect();
@@ -12,100 +12,121 @@ $timedate=date('Y-m-d H:s:i',$time);
 
 ?>
 	<div class="row">
-		<div class="col-sm-6">
-			<h2>Patient sensors</h2>
+		<div class="col-sm-3">
+			<h2>Heart rate</h2>
 			<div style="overflow:scroll;height:180px">
 			<table class="table table-hover">
 				<thead>
 					<tr>
+						<th>Gateway</th>
 						<th>Date time</th>
-						<th>Body temperature</th>
-						<th>Heartbeat</th>
+						<th>Value</th>
 					</tr>
 				</thead>
 				<tbody>
 				<tr>
 				<?php
-$today=date('Y-m-d H:s:i',time());
-$begin = new DateTime( "2016-08-18 20:00:00" );
-$end   = new DateTime($today );
-for($i = $end; $end >= $begin; $i->modify('-1 hour')){
-
-		$currdate=$i->format("YmdH");
-		$file="repository/bodytemperature-".$currdate.".txt";
-		$file2="repository/heartbeat-".$currdate.".txt";
-		if(file_exists($file))
-		{
-			echo '<tr>';
-			echo '<td>'.$i->format("Y-m-d H").':00:00</td>';
-			$myfile = fopen($file, "r");
-			echo '<td><a href="repository/bodytemperature-'.$currdate.'.txt">'.fread($myfile,filesize($file)).'°C</a></td>';
-			fclose($myfile);
-
-			$myfile = fopen($file2, "r");
-			echo '<td><a href="repository/heartbeat-'.$currdate.'.txt">'.fread($myfile,filesize($file2)).'</a></td>';
-			fclose($myfile);
-			echo '</tr>';
-		}
-	}
-				?>
+$sql="select sensordata.value, sensordata.data, gateways.name as gatewat from sensordata, gateways where name='heartbeat' order by id desc limit 50";
+$result=mysqli_query($conn,$sql);
+while($sensor=mysqli_fetch_array($result,MYSQLI_ASSOC))
+{
+	echo '<tr>';
+	echo '<td>'.$sensor[gateway],'</td>';
+	echo '<td>'.$sensor[data].'</td>';
+	echo '<td>'.$sensor[value].'°C</td>';
+	echo '</tr>';
+}
+					?>
 					</tr>
 					</tbody>
 				</table>
 			</div>
 		</div>
-		<div class="col-sm-6">
-			<h2>Ambient sensors</h2>
+		<div class="col-sm-3">
+			<h2>Body temperature</h2>
 			<div style="overflow:scroll;height:180px">
 			<table class="table table-hover">
 				<thead>
 					<tr>
+						<th>Gateway</th>
 						<th>Date time</th>
-						<th>Temperature</th>
-						<th>Humidity</th>
+						<th>Value</th>
 					</tr>
 				</thead>
 				<tbody>
 				<tr>
 				<?php
-
-$begin = new DateTime( "2016-08-18 20:00:00" );
-$end   = new DateTime($today );
-for($i = $end; $end >= $begin; $i->modify('-1 hour')){
-		echo '<tr>';
-    echo '<td>'.$i->format("Y-m-d H").':00:00</td>';
-		$currdate=$i->format("YmdH");
-		$file="repository/temperature-".$currdate.".txt";
-		$file2="repository/temperature-changed-".$currdate.".txt";
-		if(file_exists($file)&&file_exists($file2))
-		{
-			$myfile = fopen($file, "r");
-			$myfile2 = fopen($file2, "r");
-			echo '<td>';
-			echo '<a href="repository/temperature-'.$currdate.'.txt">From:'.fread($myfile,filesize($file)).'°C</a><br>';
-			echo '<a href="repository/temperature-changed-'.$currdate.'.txt">To:'.fread($myfile2,filesize($file2)).'°C</a>';
-			echo '</td>';
-			fclose($myfile);
-			fclose($myfile2);
-		}
-		else if(file_exists($file))
-		{
-			$myfile = fopen($file, "r");
-			echo '<td><a href="repository/temperature-'.$currdate.'.txt">'.fread($myfile,filesize($file)).'°C</a></td>';
-			fclose($myfile);
-		}
-
-		$file="repository/humidity-".$currdate.".txt";
-		if(file_exists($file))
-		{
-			$myfile = fopen($file, "r");
-
-			echo '<td><a href="repository/humidity-'.$currdate.'.txt">'.fread($myfile,filesize($file)).'%</a></td>';
-			fclose($myfile);
-		}
-		echo '</tr>';
-	}
-				?>
+$sql="select sensordata.value, sensordata.data, gateways.name as gatewat from sensordata, gateways where name='bodytemperature' order by id desc limit 50";
+$result=mysqli_query($conn,$sql);
+while($sensor=mysqli_fetch_array($result,MYSQLI_ASSOC))
+{
+	echo '<tr>';
+	echo '<td>'.$sensor[gateway],'</td>';
+	echo '<td>'.$sensor[data].'</td>';
+	echo '<td>'.$sensor[value].'°C</td>';
+	echo '</tr>';
+}
+					?>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<h2>Humidity</h2>
+			<div style="overflow:scroll;height:180px">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>Gateway</th>
+						<th>Date time</th>
+						<th>Value</th>
+					</tr>
+				</thead>
+				<tbody>
+				<tr>
+				<?php
+$sql="select sensordata.value, sensordata.data, gateways.name as gatewat from sensordata, gateways where name='humidity' order by id desc limit 50";
+$result=mysqli_query($conn,$sql);
+while($sensor=mysqli_fetch_array($result,MYSQLI_ASSOC))
+{
+	echo '<tr>';
+	echo '<td>'.$sensor[gateway],'</td>';
+	echo '<td>'.$sensor[data].'</td>';
+	echo '<td>'.$sensor[value].'°C</td>';
+	echo '</tr>';
+}
+					?>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<h2>Temperature</h2>
+			<div style="overflow:scroll;height:180px">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>Gateway</th>
+						<th>Date time</th>
+						<th>Value</th>
+					</tr>
+				</thead>
+				<tbody>
+				<tr>
+				<?php
+$sql="select sensordata.value, sensordata.data, gateways.name as gatewat from sensordata, gateways where name='temperature' order by id desc limit 50";
+$result=mysqli_query($conn,$sql);
+while($sensor=mysqli_fetch_array($result,MYSQLI_ASSOC))
+{
+	echo '<tr>';
+	echo '<td>'.$sensor[gateway],'</td>';
+	echo '<td>'.$sensor[data].'</td>';
+	echo '<td>'.$sensor[value].'°C</td>';
+	echo '</tr>';
+}
+					?>
 					</tr>
 					</tbody>
 				</table>
@@ -153,6 +174,7 @@ for($i = $end; $end >= $begin; $i->modify('-1 hour')){
 				<thead>
 					<tr>
 						<th>ID</th>
+						<th>Gateway</th>
 						<th>Title</th>
 						<th>Description</th>
 						<th>Employee</th>
@@ -163,15 +185,17 @@ for($i = $end; $end >= $begin; $i->modify('-1 hour')){
 				<tbody>
 				<tr>
 				<?php
-	$sql="select report.title,report.reportid, report.description, report.creationdate, report.changedate, report.status, users.username from report, users
+	$sql="select report.title,report.reportid, report.description, gateways.name as gateway, report.creationdate, report.changedate, report.status, users.username from report, users, gateways
 	where users.userid=report.userid
-	and report.status='Finished'  order by report.reportid desc";
+	and report.status='Finished'
+	and gateways.gatewayid=report.gatewayid
+	order by report.reportid desc";
 
 	$result=mysqli_query($conn,$sql);
 	while($report=mysqli_fetch_array($result,MYSQLI_ASSOC)){
 		echo '<tr>';
 		echo '<td>#'.$report[reportid].'</td>';
-		echo '<td>'.$report[title].'</td>';
+		echo '<td>'.$report[gateway].'</td>';
 		echo '<td>'.$report[title].'</td>';
 		echo '<td>'.$report[description].'</td>';
 		echo '<td>'.$report[username].'</td>';
